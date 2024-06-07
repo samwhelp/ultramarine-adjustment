d71539813c3f1f5a47e3919e9b27d32d2f327701
samwhelp <samwhelp@users.noreply.github.com>
2024-06-07 14:52:39 +0800

    fix

---
+++ prototype/main/lightdm-config/lightdm-gtk-greeter/theme-selector/profile/Materia-Dark/README.md
@@ -0,0 +1,89 @@
+
+# lightdm / lightdm-gtk-greeter / config / theme / Arc-Dark
+
+
+
+
+## Config File Path
+
+### lightdm-gtk-greeter
+
+| Config File Path |
+| --- |
+| [/etc/lightdm/lightdm-gtk-greeter.conf.d/70_theme.conf](./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/70_theme.conf) |
+
+
+
+
+## Fedora Package
+
+> For LightDM and LightDM Gtk Greeter
+
+| Fedora Package |
+| --- |
+| [lightdm](https://packages.fedoraproject.org/pkgs/lightdm/lightdm/) |
+| [lightdm-gtk](https://packages.fedoraproject.org/pkgs/lightdm-gtk/lightdm-gtk/) |
+
+> For `theme-name =`
+
+| Fedora Package |
+| --- |
+| [materia-gtk-theme](https://packages.fedoraproject.org/pkgs/materia-gtk-theme/materia-gtk-theme/) |
+
+
+> For `icon-theme-name  =`
+
+| Fedora Package |
+| --- |
+| [papirus-icon-theme](https://packages.fedoraproject.org/pkgs/papirus-icon-theme/papirus-icon-theme/) |
+
+
+> For `cursor-theme-name =`
+
+| Fedora Package |
+| --- |
+| [breeze-cursor-theme](https://packages.fedoraproject.org/pkgs/plasma-breeze/breeze-cursor-theme/) |
+
+
+
+
+## Howto
+
+
+### install
+
+``` sh
+./install.sh
+```
+
+or
+
+``` sh
+make install
+```
+
+
+### package-install
+
+``` sh
+./package-install.sh
+```
+
+or
+
+``` sh
+make package-install
+```
+
+
+### config-install
+
+``` sh
+./config-install.sh
+```
+
+or
+
+``` sh
+make config-install
+```
---
+++ prototype/main/lightdm-config/lightdm-gtk-greeter/theme-selector/profile/Materia-Dark/config-install.sh
@@ -0,0 +1,227 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: lightdm
+##
+
+lightdm_config_install () {
+
+	echo
+	echo
+	echo
+	echo
+	echo "##"
+	echo "## Config: lightdm"
+	echo "##"
+	echo
+
+
+	#lightdm_config_install_by_dir
+
+	lightdm_config_install_by_each_file
+
+	echo
+
+}
+
+
+
+lightdm_config_install_by_dir () {
+
+	echo
+	echo "sudo install -dm755 /etc/lightdm"
+	sudo install -dm755 "/etc/lightdm"
+
+	echo
+	echo "sudo cp -rf ./asset/overlay/etc/lightdm/. /etc/lightdm"
+	sudo cp -rf "./asset/overlay/etc/lightdm/." "/etc/lightdm"
+
+}
+
+lightdm_config_install_by_each_file () {
+
+
+	#lightdm_conf_install_by_each_file
+	#lightdm_conf_d_install_by_each_file
+
+
+	#lightdm_gtk_greeter_conf_install_by_each_file
+	lightdm_gtk_greeter_conf_d_install_by_each_file
+
+
+	return 0
+
+}
+
+lightdm_conf_install_by_each_file () {
+
+
+	echo
+	echo "sudo install -dm755 /etc/lightdm"
+	sudo install -dm755 "/etc/lightdm"
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm.conf" "/etc/lightdm/lightdm.conf"
+
+
+	return 0
+
+}
+
+lightdm_conf_d_install_by_each_file () {
+
+
+	echo
+	echo "sudo install -dm755 /etc/lightdm/lightdm.conf.d"
+	sudo install -dm755 "/etc/lightdm/lightdm.conf.d"
+
+	#lightdm_conf_d_install_by_each_file_for_basic
+	#lightdm_conf_d_install_by_each_file_for_greeter
+
+
+	return 0
+
+}
+
+lightdm_conf_d_install_by_each_file_for_basic () {
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm.conf.d/50_basic.conf /etc/lightdm/lightdm.conf.d/50_basic.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm.conf.d/50_basic.conf" "/etc/lightdm/lightdm.conf.d/50_basic.conf"
+
+
+	return 0
+
+}
+
+
+lightdm_conf_d_install_by_each_file_for_greeter () {
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm.conf.d/70_greeter.conf /etc/lightdm/lightdm.conf.d/70_greeter.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm.conf.d/70_greeter.conf" "/etc/lightdm/lightdm.conf.d/70_greeter.conf"
+
+
+	return 0
+
+}
+
+lightdm_gtk_greeter_conf_install_by_each_file () {
+
+
+	echo
+	echo "sudo install -dm755 /etc/lightdm"
+	sudo install -dm755 "/etc/lightdm"
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf" "/etc/lightdm/lightdm-gtk-greeter.conf"
+
+
+	return 0
+
+}
+
+lightdm_gtk_greeter_conf_d_install_by_each_file () {
+
+	echo
+	echo "sudo install -dm755 /etc/lightdm/lightdm-gtk-greeter.conf.d"
+	sudo install -dm755 "/etc/lightdm/lightdm-gtk-greeter.conf.d"
+
+	#lightdm_gtk_greeter_conf_d_install_by_each_file_for_basic
+	#lightdm_gtk_greeter_conf_d_install_by_each_file_for_panel
+	lightdm_gtk_greeter_conf_d_install_by_each_file_for_theme
+	#lightdm_gtk_greeter_conf_d_install_by_each_file_for_background
+
+
+	return 0
+
+}
+
+lightdm_gtk_greeter_conf_d_install_by_each_file_for_basic () {
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/50_basic.conf /etc/lightdm/lightdm-gtk-greeter.conf.d/50_basic.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/50_basic.conf" "/etc/lightdm/lightdm-gtk-greeter.conf.d/50_basic.conf"
+
+
+	return 0
+
+}
+
+
+lightdm_gtk_greeter_conf_d_install_by_each_file_for_panel () {
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/60_panel.conf /etc/lightdm/lightdm-gtk-greeter.conf.d/60_panel.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/60_panel.conf" "/etc/lightdm/lightdm-gtk-greeter.conf.d/60_panel.conf"
+
+
+	return 0
+
+}
+
+lightdm_gtk_greeter_conf_d_install_by_each_file_for_theme () {
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/70_theme.conf /etc/lightdm/lightdm-gtk-greeter.conf.d/70_theme.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/70_theme.conf" "/etc/lightdm/lightdm-gtk-greeter.conf.d/70_theme.conf"
+
+
+	return 0
+
+}
+
+lightdm_gtk_greeter_conf_d_install_by_each_file_for_background () {
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/80_background.conf /etc/lightdm/lightdm-gtk-greeter.conf.d/80_background.conf"
+	sudo install -Dm644 "./asset/overlay/etc/lightdm/lightdm-gtk-greeter.conf.d/80_background.conf" "/etc/lightdm/lightdm-gtk-greeter.conf.d/80_background.conf"
+
+
+	return 0
+
+}
+
+##
+### Tail: lightdm
+################################################################################
+
+
+################################################################################
+### Head: config_install
+##
+
+main_config_install () {
+
+	lightdm_config_install
+
+}
+
+##
+### Tail: config_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_config_install
+
+}
+
+##
+## Start
+##
+__main__
+
+##
+### Tail: Main
+################################################################################
