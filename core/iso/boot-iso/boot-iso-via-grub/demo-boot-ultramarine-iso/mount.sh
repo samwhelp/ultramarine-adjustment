168016d0dec0c561c7e0ed825e94188cffc4e9a7
samwhelp <samwhelp@users.noreply.github.com>
2025-11-25 17:58:56 +0800

    fix

--- core/iso/boot-iso/boot-iso-via-grub/demo-boot-ultramarine-iso/mount.sh
+++ core/iso/boot-iso/boot-iso-via-grub/demo-boot-ultramarine-iso/mount.sh
@@ -8,10 +8,10 @@ mkdir -p mnt
 
 
 
-sudo mount -o loop ultramarine-xfce-43-live-anaconda-x86_64.iso mnt
+sudo mount -o loop ultramarine-xfce-43-live-x86_64.iso mnt
 
-#sudo mount -o loop ultramarine-plasma-43-live-anaconda-x86_64.iso mnt
+#sudo mount -o loop ultramarine-plasma-43-live-x86_64.iso mnt
 
-#sudo mount -o loop ultramarine-budgie-43-live-anaconda-x86_64.iso mnt
+#sudo mount -o loop ultramarine-budgie-43-live-x86_64.iso mnt
 
-#sudo mount -o loop ultramarine-gnome-43-live-anaconda-x86_64.iso mnt
+#sudo mount -o loop ultramarine-gnome-43-live-x86_64.iso mnt
