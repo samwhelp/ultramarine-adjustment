352aee16ce98a44b2f8994c5f097673d172b0813
samwhelp <samwhelp@users.noreply.github.com>
2025-11-25 12:52:33 +0800

    fix

---
+++ core/iso/boot-iso/boot-iso-via-grub/demo-boot-ultramarine-43-iso/mount.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+
+
+mkdir -p mnt
+
+
+
+
+sudo mount -o loop ultramarine-xfce-43-live-x86_64.iso mnt
+
+#sudo mount -o loop ultramarine-plasma-43-live-x86_64.iso mnt
+
+#sudo mount -o loop ultramarine-budgie-43-live-x86_64.iso mnt
+
+#sudo mount -o loop ultramarine-gnome-43-live-x86_64.iso mnt
